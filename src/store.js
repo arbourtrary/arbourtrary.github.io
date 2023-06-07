088158997b38046219214f79a2d054d8236f54ac
arbourtrary <arbourtrary@gmail.com>
2023-06-07 08:30:55 -1000

    Attempting to make the full website sort of SSR - need this for SEO purposes, but really im gonna hydrate on the fly ;)

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store'
 
 export const section = writable(0)
-export const isPortrait = writable(window.innerWidth < 1000);
+export const isPortrait = writable("");
 export const bgColor = writable("");
 
 export const textColor1 = writable("");
