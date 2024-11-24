427ec378b54e2dbf9b1a1e8c21a958f50eedf399
David Newcomb <davidnewcomb@Davids-MBP.fritz.box>
2024-11-23 21:23:03 -1000

    Scaffolding for interactive fisheye lines

---
+++ src/components/FisheyeLines.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let lineWidth = 1;
+	export let values = [];
+	export let colors = [];
+</script>
+
+<div class="line-container">
+	{#each values as value, index}
+		<div class="line" style={colors.length ? `background: ${colors[index]}` : ""}></div>
+	{/each}
+</div>
+
+<style>
+	.line-container {
+		display: flex;
+		align-items: center;
+		gap: 2px;
+		cursor: pointer;
+		height: 50px;
+	}
+	.line {
+		height: 35px;
+		width: 2px;
+		opacity: 0.75;
+	}
+	.line:hover {
+		width: 2px;
+		height: 50px;
+		opacity: 1;
+	}
+</style>
