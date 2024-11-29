1778482cd47d7edb3f06697bbee3516e1643a141
David Newcomb <davidnewcomb@Davids-MBP.fritz.box>
2024-11-29 11:34:07 -1000

    Fixing dates

--- src/routes/sketches/[slug]/+page.js
+++ src/routes/sketches/[slug]/+page.js
@@ -4,7 +4,7 @@ import { loadJSON } from '../../../utils/file.js'
 
 const sketches = [
   {
-    "date": "11/19/24",
+    "date": "11/17/24",
     "title": "Coalescing Text", 
     "slug": "coalescing-text",
     "description": "Code sketch animating letters - words coalescing slowly, evolving, synaesthetic, dissolving",
@@ -15,7 +15,7 @@ const sketches = [
     "socialImage": "https://arbourtrary.com/images/social_sketch_0_viz.jpg"
   },
   {
-    "date": "12/03/24",
+    "date": "11/24/24",
     "title": "Polygon or Circle?", 
     "slug": "polygon-or-circle",
     "description": "The Platonic circle does not exist, but at what point can we no longer tell the difference?",
@@ -26,7 +26,7 @@ const sketches = [
     "socialImage": "https://arbourtrary.com/images/social_sketch_1_viz.jpg"
   },
   {
-    "date": "11/26/24",
+    "date": "11/29/24",
       "title": "Direct Etymologies", 
       "slug": "direct-etymologies",
       "description": "Scraping my own website to find etymologies of words that are the result of a direct lineage",
