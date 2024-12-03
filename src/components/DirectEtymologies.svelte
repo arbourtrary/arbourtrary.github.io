4d12a622ced44e757c24d7ee2799f03af3204b06
David Newcomb <davidnewcomb@Mac.fritz.box>
2024-12-02 19:15:43 -1000

    Changing etymology to random looping selection without replacement

--- src/components/DirectEtymologies.svelte
+++ src/components/DirectEtymologies.svelte
@@ -3,20 +3,32 @@
 	import { loadJSON } from "../utils/file.js";
 	import { languageToColor } from "../utils/linguistics.js"
 
+	let fixedData;
 	let data;
 	let words;
 	let opacity = 0;
 	const ETYMOLOGIES_FILEPATH = "/data/sketches/direct-etymologies.json"
 
 	onMount(async () => {
-		data = await loadJSON(ETYMOLOGIES_FILEPATH);
-		words = getRandomEntry(data);
+		fixedData = await loadJSON(ETYMOLOGIES_FILEPATH);
+		data = JSON.parse(JSON.stringify(fixedData));
+		setRandomEntryWithoutReplacement();
 	});
 
 	function getRandomEntry(dict) {
 		const keys = Object.keys(dict);
 		const randomIndex = Math.round(Math.random() * (keys.length - 1));
 		return dict[keys[randomIndex]];
+	}
+
+	function setRandomEntryWithoutReplacement() {
+		const keys = Object.keys(data);
+		const randomIndex = Math.round(Math.random() * (keys.length - 1));
+		words = data[keys[randomIndex]];
+		delete data[keys[randomIndex]];
+		if (Object.keys(data).length === 0) {
+			data = JSON.parse(JSON.stringify(fixedData));
+		}
 	}
 </script>
 
@@ -32,7 +44,7 @@
 		{/each}
 		</div>
 		<div class="button-container">
-			<button on:click={() => { words = getRandomEntry(data); }}>
+			<button on:click={() => { setRandomEntryWithoutReplacement(); }}>
 				<svg xmlns="http://www.w3.org/2000/svg" height="22px" fill="currentColor"viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.6 9.6 0 0 0 7.556 8a9.6 9.6 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.6 10.6 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.6 9.6 0 0 0 6.444 8a9.6 9.6 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5"/>
 					<path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192"/>
