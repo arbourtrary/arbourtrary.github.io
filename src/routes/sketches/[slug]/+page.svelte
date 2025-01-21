0551aff85e9934ce2bcd452233c9dd71689532dc
arbourtrary <arbourtrary@gmail.com>
2025-01-20 15:12:23 -1000

    Fetching content on prev/next

--- src/routes/sketches/[slug]/+page.svelte
+++ src/routes/sketches/[slug]/+page.svelte
@@ -20,12 +20,15 @@
     let nextSketch;
     let content = "";
     let body;
+    let response;
+
+    async function fetchContent(filename) {
+        response = await fetch(filename)
+        content = await response.text();
+    }
 
     onMount(async () => {
-        const response = await fetch(data.sketch.filename)
-        sketch = data.sketch;
-        sketchIndex = sketch.index;
-        content = await response.text();
+        fetchContent(data.sketch.filename)
         body = document.body;
         document.documentElement.style.setProperty('--highlight', data.sketch.highlight);
     })
@@ -108,7 +111,7 @@
 </div>
 <div class="more">
     {#if prevSketch}
-        <a href={`/sketches/${prevSketch.slug}`} style="margin-right: 15px;">
+        <a on:click={fetchContent(prevSketch.filename)} href={`/sketches/${prevSketch.slug}`} style="margin-right: 15px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor">
               <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
             </svg>
@@ -116,7 +119,7 @@
         </a>
     {/if}
     {#if nextSketch}
-        <a href={`/sketches/${nextSketch.slug}`} style="margin-left: 15px;">
+        <a on:click={fetchContent(nextSketch.filename)} href={`/sketches/${nextSketch.slug}`} style="margin-left: 15px;">
             <p style="text-align: right;">{nextSketch.title}</p>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
@@ -338,10 +341,8 @@
         color: var(--color-1);
     }
     .more a:hover p {
-        background: var(--color-3);
-        text-decoration: none;
-        border-radius: 5px;
-        color: white;
+        text-decoration: underline;
+        color: var(--black);
     }
     .more p {
         margin: 0px;
