c1287732ad293b5a8a6213c2881b1ff01fab3193
David Newcomb <dnewcomb@mcclatchy.com>
2025-02-14 11:01:47 -1000

    Expanding RSS feed

--- src/routes/feed/rss.xml/+server.js
+++ src/routes/feed/rss.xml/+server.js
@@ -13,18 +13,57 @@ async function getText(event, filepath) {
   return text;
 }
 
+async function getJSON(event, filepath) {
+  const response = await event.fetch(filepath);
+  const text = await response.json();
+  return text;
+}
+
 export async function GET(event) {
   event.setHeaders({
     'Cache-Control': `max-age=0, s-max-age=3600`,
     'Content-Type': 'application/xml'
   })
 
-  const response = await event.fetch('/data/writings.json');
-  const writings = await response.json();
-  // get them most recent first
-  writings.reverse();
-  const texts = await Promise.all(writings.map(writing => getText(event, writing.filename)));
-  const markedTexts = texts.map(text => marked(text));
+  // writings
+  const responseWriting = await event.fetch('/data/writings.json');
+  const writings = await responseWriting.json();
+  writings.forEach(writing => writing.category = "Article");
+  writings.forEach(writing => writing.route = "writings");
+  const writingTexts = await Promise.all(writings.map(writing => getText(event, writing.filename)));
+  const writingMarkedTexts = writingTexts.map(writingText => marked(writingText));
+  writings.forEach((writing,index) => writing.marked = writingMarkedTexts[index]);
+
+  // sketches
+  const responseSketch = await event.fetch('/data/sketches.json');
+  const sketches = await responseSketch.json();
+  sketches.forEach(sketch => sketch.category = "Sketch");
+  sketches.forEach(sketch => sketch.route = "sketches");
+  const sketchTexts = await Promise.all(sketches.map(sketch => getText(event, sketch.filename)));
+  const sketchMarkedTexts = sketchTexts.map(sketchText => marked(sketchText));
+  sketches.forEach((sketch,index) => sketch.marked = sketchMarkedTexts[index]);
+
+  // poems
+  const responsePoem = await event.fetch('/data/poems.json');
+  const poems = await responsePoem.json();
+  poems.forEach(poem => poem.category = "Poem");
+  poems.forEach(poem => poem.route = "poems");
+  const poemTexts = await Promise.all(poems.map(poem => getJSON(event, `/data/poems/${poem.slug}.json`)));
+  const poemMarkedTexts = poemTexts.map(poem => marked(poem["text"].replaceAll("/","")));
+  poems.forEach((poem,index) => poem.marked = poemMarkedTexts[index]);
+
+  // Function to sort array by date
+  function sortArrayByDate(array) {
+      return array.sort((a, b) => {
+          const dateA = new Date(a.date);
+          const dateB = new Date(b.date);
+          return dateA - dateB;
+      });
+  }
+
+  let items = writings.concat(poems).concat(sketches);
+  items = sortArrayByDate(items).reverse();
+
   const xml = `<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
       <channel>
         <title>Arbourtrary RSS Feed</title>
@@ -40,20 +79,21 @@ export async function GET(event) {
             <title>Arbourtrary RSS Feed</title>
             <link>https://arbourtrary.com</link>
         </image>
-        ${writings
+        ${items
           .map(
-            (writing, index) =>
-              `
-              <item>
-                <guid isPermaLink="false">https://arbourtrary.com/writings/${writing.slug}</guid>
-                <title>${writing.title}</title>
-                <description><![CDATA[${markedTexts[index]}]]></description>
-                <link>https://arbourtrary.com/writings/${writing.slug}</link>
-                <pubDate>${new Date(writing.date).toUTCString()}</pubDate>
-                <author>arbourtrary@gmail.com</author>
-            </item>
-          `
-          )
+            (item) => {
+              return `
+                <item>
+                  <guid isPermaLink="false">https://arbourtrary.com/${item.route}/${item.slug}</guid>
+                  <title>${item.title || item.name}</title>
+                  <description><![CDATA[${item.marked}]]></description>
+                  <link>https://arbourtrary.com/${item.route}/${item.slug}</link>
+                  <pubDate>${new Date(item.date).toUTCString()}</pubDate>
+                  <category>${item.category}</category>
+                  <author>arbourtrary@gmail.com</author>
+                </item>
+              `  
+          })
           .join('')}
       </channel>
     </rss>`
