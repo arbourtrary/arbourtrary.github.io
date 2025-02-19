84f93786ca574bd9b842c71655903ab9b8b10007
David Newcomb <dnewcomb@mcclatchy.com>
2025-02-19 11:49:53 -1000

    Trying to fix urls with base

--- src/routes/writings/[slug]/+page.svelte
+++ src/routes/writings/[slug]/+page.svelte
@@ -111,7 +111,7 @@
 </div>
 <div class="more">
     {#if data.writing.prev}
-        <a href={`/writings/${data.writing.prev.slug}`} style="margin-right: 15px;">
+        <a href={base + `/writings/${data.writing.prev.slug}`} style="margin-right: 15px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor">
               <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
             </svg>
@@ -119,7 +119,7 @@
         </a>
     {/if}
     {#if data.writing.next}
-        <a href={`/writings/${data.writing.next.slug}`} style="margin-left: 15px;">
+        <a href={base + `/writings/${data.writing.next.slug}`} style="margin-left: 15px;">
             <p style="text-align: right;">{data.writing.next.title}</p>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
