83782fcb52f2df60f0fcaa3e9cb23baaffd4d2de
arbourtrary <arbourtrary@gmail.com>
2025-03-24 13:58:59 -1000

    Fixing opacity

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -260,6 +260,9 @@
 /*      url("data:image/svg+xml,%3Csvg viewBox='0 0 347 347' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='3.74' numOctaves='20' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")*/
       pointer-events: none;
   }
+  :global(.image-container.active) {
+    opacity: 1;
+  }
   :global(.image-container.active .filtered-image) {
     filter: blur(0px);
   }
